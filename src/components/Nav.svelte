d2f31ea54cd0c21b6a3260e6dbfeeb9b0f31caaf
jdkfx <amateurranch@gmail.com>
2021-03-26 02:29:02 +0900

    :art: ナビバーなどのレスポンシブ対応

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -4,6 +4,7 @@
 
 <style>
 	nav {
+		box-shadow: 0 10px 25px 0 rgba(0, 0, 0, .5);
 		background-color: #ff6316;
 		font-family: "MS Pゴシック",sans-serif;
 		height: 100%;
@@ -36,8 +37,23 @@
 		text-decoration: none;
 	}
 
-	.my-link {
+	.top-php {
+		font-size: 2em;
+		font-weight: bold;
+	}
+
+	.title {
+		font-size: 2em;
+		font-weight: bold;
+	}
+
+	.size18 {
+		font-size: 1.8em;
+	}
+
+	li .my-link a {
 		/* margin: 0 auto; */
+		display: inline-block;
 		text-align: center;
 	}
 
@@ -45,20 +61,52 @@
 		color: #ffffff;
 		padding: 0.3em;
 	}
+
+	@media screen and ( max-width:480px )
+	{
+		nav {
+			box-shadow: 0 10px 25px 0 rgba(0, 0, 0, .2);
+			font-size: 1em ;
+			height: 15%;
+			position: fixed;
+			width: 100%;
+			z-index: 10;
+		}
+
+		ul {
+			padding-top: 0;
+			text-align: center;
+		}
+
+		li {
+			display: inline-block;
+			font-size: 0.7em;
+		}
+
+		.top-php {
+			display: none;
+		}
+
+		.title {
+			display: block;
+		}
+	}
 </style>
 
 <nav>
 	<ul>
-		<li class="top-php" style="font-color: #ffffff;font-size: 2em;font-weight: bold;">&lt;?php</li>
-		<li><a style="font-size: 2em;font-weight: bold;" aria-current="{segment === undefined ? 'page' : undefined}" href=".">jdkfx.com</a></li>
-		<li><a style="font-size: 1.8em;" aria-current="{segment === 'about' ? 'page' : undefined}" href="about">$about</a></li>
+		<li class="top-php">&lt;?php</li>
+		<li class="title"><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">jdkfx.com</a></li>
+		<li><a class="size18" aria-current="{segment === 'about' ? 'page' : undefined}" href="about">$about</a></li>
 
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a style="font-size: 1.8em;" rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">$blogs</a></li>
-		<div class="my-link">
-			<a href="https://twitter.com/jdkfx" target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter fa-3x"></i></a>
-			<a href="https://github.com/jdkfx" target="_blank" rel="noopener noreferrer"><i class="fab fa-github fa-3x"></i></a>
-		</div>
+		<li><a class="size18" rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">$blogs</a></li>
+		<li>
+			<div class="my-link">
+				<a href="https://twitter.com/jdkfx" target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter fa-3x"></i></a>
+				<a href="https://github.com/jdkfx" target="_blank" rel="noopener noreferrer"><i class="fab fa-github fa-3x"></i></a>
+			</div>
+		</li>
 	</ul>
 </nav>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -10,10 +10,21 @@
 		/* max-width: 56em; */
 		width: 80%;
 		background-color: white;
+		overflow: hidden;
 		padding: 2em;
 		/* margin: 0 auto; */
 		margin-left: auto;
 		box-sizing: border-box;
+	}
+
+	@media screen and ( max-width:480px )
+	{
+		main {
+			font-size: 1em ;
+			width: 100%;
+			padding: 1em;
+			margin: 0 auto;
+		}
 	}
 </style>
 
